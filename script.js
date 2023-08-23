c8e5492986cc58be4c9d26f26020de0c21b6927e
Tanmayjain0906 <tanmayjain983@gmail.com>
2023-08-23 17:42:13 +0530

    complete

--- script.js
+++ script.js
@@ -1,39 +1,127 @@
 const sidebar = document.getElementsByClassName("yt-sidebar")[0];
 const slider = document.getElementsByClassName("ham-bar")[0]
 
+const filter = document.getElementsByClassName("filters")[0];
+
+const videoContainer = document.getElementsByClassName("video-container")[0];
+
 slider.addEventListener("click", () => {
     sidebar.classList.toggle("small-slidebar");
+    filter.classList.toggle("big-filter");
+    videoContainer.classList.toggle("big-video-container");
 })
 
 
 
 
-// let apiKey = "AIzaSyDUsEjNCN-m2gDf5yj5pOy6ovTWIxmGa-g";
-// let baseUrl = "https://www.googleapis.com/youtube/v3";
+let apiKey = "AIzaSyDUsEjNCN-m2gDf5yj5pOy6ovTWIxmGa-g";
+let baseUrl = "https://www.googleapis.com/youtube/v3";
 
-// let searchButton = document.getElementById("search");
-// let searchInput = document.getElementById("search-input");
+let searchButton = document.getElementById("search");
+let searchInput = document.getElementById("search-input");
 
-// searchButton.addEventListener("click", ()=> {
+searchButton.addEventListener("click", () => {
 
-//     let result = searchInput.value.trim();
+    let result = searchInput.value.trim();
 
-//     if(result === "")
-//     {
-//         return;
-//     }
+    if (result === "") {
+        return;
+    }
 
-//     getSearchResult(result);
-// })
+    getSearchResult(result);
+})
 
-// async function getSearchResult(result)
-// {
+async function getSearchResult(result) {
 
-//     let url = `${baseUrl}/search?key=${apiKey}&q=${result}&part=snippet&maxResults=10`;
-     
+    let url = `${baseUrl}/search?key=${apiKey}&type=video&q=${result}&part=snippet&maxResults=25`;
 
-//     let response = await fetch(url);
-//     let data = await response.json();
+    let response = await fetch(url);
+    let data = await response.json();
 
-//     console.log(data);
-// }
+
+    addDataToUI(data.items);
+}
+
+let defaultSearch = "multiverse";
+
+getSearchResult(defaultSearch);
+
+
+function addDataToUI(data) {
+    videoContainer.innerHTML = "";
+
+    data.forEach(element => {
+
+        let channel = fetchChannelDetails(element.snippet.channelId);
+        channel.then((channelData) => {
+
+            let div = document.createElement("div");
+            div.className = "video-card";
+
+            div.innerHTML = `<div class="upper">
+        <img src="${element.snippet.thumbnails.high.url}" class="thumbnial">
+    </div>
+
+    <div class="lower">
+
+        <div class="channel-container">
+
+            <img src="${channelData.items[0].snippet.thumbnails.default.url
+                }"" class="channel-logo">
+
+            <div class="right">
+
+                <p class="title-info">${element.snippet.title}</p>
+
+                <div class="channel-name">
+                    ${element.snippet.channelTitle}
+                </div>
+                   
+                <div class="channel-data">
+                    <p class="views">${formatNumber(
+                        channelData.items[0].statistics.viewCount
+                      )} views</p>
+
+                    <p class="post-time">. ${new Date(
+                    channelData.items[0].snippet.publishedAt
+                ).toLocaleDateString()}</p>
+                </div>
+
+            </div>
+        </div>
+        
+    </div>`
+
+            videoContainer.appendChild(div);
+        })
+    });
+}
+
+
+async function fetchChannelDetails(data) {
+    let url = `${baseUrl}/channels?part=snippet,statistics,contentDetails&id=${data}&key=${apiKey}`;
+
+    let response = await fetch(url);
+    let result = await response.json();
+
+    return result;
+}
+
+function formatNumber(num, precision = 2) {
+    const map = [
+      { suffix: "T", threshold: 1e12 },
+      { suffix: "B", threshold: 1e9 },
+      { suffix: "M", threshold: 1e6 },
+      { suffix: "K", threshold: 1e3 },
+      { suffix: "", threshold: 1 },
+    ];
+  
+    const found = map.find((x) => Math.abs(num) >= x.threshold);
+    if (found) {
+      const formatted = (num / found.threshold).toFixed(precision) + found.suffix;
+  
+      return formatted;
+    }
+  
+    return num;
+  }
